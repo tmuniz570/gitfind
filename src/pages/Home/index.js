a0a3672710b7f2695e1a4481856486dcaed9eee5
Thiago Brandao <tmuniz570@gmail.com>
2024-08-22 00:30:43 +0100

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,64 @@
+import { useState } from "react";
+import { Header } from "../../components/Header";
+import background from '../../assets/background.png';
+import ItemList from "../../components/ItemList";
+import "./styles.css"
+
+function App() {
+  const [user, setUser] = useState("");
+  const [currentUser, setCurrentUser] = useState(null);
+  const [repos, setRepos] = useState(null);
+
+  const handleGetData = async () => {
+    const userData = await fetch(`https://api.github.com/users/${user}`);
+    const newUser = await userData.json();
+
+    if (newUser.name) {
+      const { avatar_url, name, bio, login } = newUser;
+      setCurrentUser({ avatar_url, name, bio, login });
+
+      const reposData = await fetch(`https://api.github.com/users/${user}/repos`);
+      const newRepos = await reposData.json();
+      if (newRepos.length) {
+        setRepos(newRepos);
+      }
+    }
+  }
+
+  return (
+    <div className="App">
+      <Header />
+      <div className="content">
+        <img src={background} className="background" alt="backphoto" />
+        <div className="info">
+          <div>
+            <input name="user" value={user} onChange={event => setUser(event.target.value)} placeholder="@username" />
+            <button onClick={handleGetData}>Search</button>
+          </div>
+          {currentUser?.name ? (<>
+            <div className="profile">
+              <img src={currentUser.avatar_url} className="photo" alt="Profile" />
+              <div>
+                <h3>{currentUser.name}</h3>
+                <span>@{currentUser.login}</span>
+                <p>{currentUser.bio}</p>
+              </div>
+            </div>
+            <hr />
+
+          </>) : null}
+          {repos?.length ? (<>
+            <div>
+              <h4>Repository</h4>
+              {repos.map(repo => (
+                <ItemList title={repo.name} description={repo.description} />
+              ))}
+            </div>
+          </>) : null}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
